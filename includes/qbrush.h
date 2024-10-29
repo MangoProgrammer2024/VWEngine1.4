3ec9d1125d2b52d56fe19b8431908c724f2fa717
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 16:57:05 -0700

    Create qbrush.h

---
+++ includes/qbrush.h
@@ -0,0 +1,35 @@
+//qbrush.h
+#ifndef QBRUSH_H
+#define QBRUSH_H
+
+typedef class qbrush{
+public:
+
+  int qNumberid = 0;
+
+    qbrush();
+      ~qbrush();
+
+  enum qb_primit_mode{ brush_free_mode = 0, brush_lock_mode = 1, brush_save_mode = 2 };
+
+    typedef bool qboolean;
+
+    qboolean qbCheckMode(qbrush brush);  qboolean qbCheck_BrushVariable(qbrush * qVariable);
+      const char * qb_GlobalBrushName(qbrush * current_qbrush)const;
+
+      qbrush * brushpoints( int qbPoints[8] );
+      void qbrush_Alloc(std::size_t * qbrushSize);
+      qbrush * qepair(qbrush * q, const char * key, const char * value);
+      void split_brush(qbrush * q, int addPoints, qboolean qSplitfaces);
+      void brush_AddToQTable(qbrush * current_qbrush, qbrushtable qtable);
+        qboolean CreateBrush(qbrush brush);
+
+        virtual void Brush_DrawToGrid(qbru\]1q]\qw1l;\=-riiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii);
+};
+
+typedef struct qbrushtable{
+
+
+};
+
+#endif
