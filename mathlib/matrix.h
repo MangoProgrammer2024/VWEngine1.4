5ee05699addb9889e5cebfeb697b0e5a47240fc7
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 13:58:35 -0700

    Update matrix.h

--- mathlib/matrix.h
+++ mathlib/matrix.h
@@ -14,7 +14,30 @@ typedef class matrix_m{
   mcase operator %^(matrix_m& m, float rotation);
   mcase operator %&(matrix_m& m, float scale);
 
+  //matrix axis
+  float matrix_x[4];
+  float matrix_y[4];
+  float matrix_z[4];
 
+   //matrix variable
+   typedef float matrix_var;
+
+   //matrix functions
+    enum{
+     TRANSLATE = 0,
+     ROTATE = 1,
+     SCALE = 2
+    }MatrixMode;
+
+   //matrix identity
+   virtual void matrix_identity(matrix_m& mId, float axis[4], matrix_m * function);
+   //matrix mode
+   void matrix_mode(matrix_m& m, matrix_m * mode);
+
+   #define MATRIX_X_AXIS 000x000
+   #define MATRIX_Y_AXIS 111x111
+   #define MATRIX_Z_AXIS 222x222
+   
 };
 
 #endif
