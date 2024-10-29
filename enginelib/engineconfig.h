5ae1449e1a1f8e103eb2e50392f2bc0737fbe8a7
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 15:15:07 -0700

    Update engineconfig.h

--- enginelib/engineconfig.h
+++ enginelib/engineconfig.h
@@ -1,1 +1,34 @@
+//engineconfig.h
+#ifndef ENGINECONFIG_H
+#define ENGINECONFIG_H
 
+#include <string>
+
+class config{
+public:
+std::string engine_path;
+std::string estr;
+
+  bool loadcontents(config);
+
+  //string engine config
+  void strconfig(config * str, config& engine-config)
+  {
+    if(str->estr = engine_path)
+    {
+    engine-config.loadcontents(config) = true;
+      if(!str->estr = engine_path)
+     {
+       engine-config.loadcontents(!config) = false;
+     }
+    }    
+  };
+
+
+
+
+};
+
+
+
+#endif
