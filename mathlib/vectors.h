32a54da8637ebc23ed2c80275d3bc27eb991acc0
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 10:55:08 -0500

    Update vectors.h

--- mathlib/vectors.h
+++ mathlib/vectors.h
@@ -1,1 +1,20 @@
+//vectors.h
+#pragma once
+#ifndef VECTORS_H
+#define VECTORS_H
 
+template<typename vec>class vector
+
+ public:
+
+ // operator + has float value
+ // operator - has unsigned int value
+ // operator * has int value
+ // operator % has boolean value
+
+ vec operator + (vector, float v);
+ vec operator - (vector, unsigned int vsub);
+ vec operator * (vector, int vscalar);
+ vec operator % (vector, bool vscale);
+
+#endif
