45d8d18e5aa9d9bf7c6b7efaed27ce1f12a6f8fa
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 18:52:37 -0700

    Create qmenubar.h

---
+++ includes/qmenubar.h
@@ -0,0 +1,21 @@
+//qmenubar.h
+#ifndef QMENUBAR_H
+#define QMENUBAR_H
+
+#include <QWidget.h>
+
+//create menu
+QAction * construct_menu_bar(QWidget * menu, const char * menuname, int menu_items[]);
+QMenu * qWinParent(QWidget * qParent);
+  //possible menu items
+  const char * fileitem = "File";
+  const char * edititem = "Edit";
+  const char * miscitem = "Misc";
+  const char * textureitem = "Texture";
+  const char * griditem = "Grid";
+  const char * bspitem = "Bsp";
+  const char * brushitem = "Brush";
+  const char * patchitem = "Patch";
+  const char * pluginitem = "Plugin";
+
+#endif
