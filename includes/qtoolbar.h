197f819d1beca4cebe6d4040fbafb50bc87db9dc
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-30 10:58:35 -0500

    Create qtoolbar.h

---
+++ includes/qtoolbar.h
@@ -0,0 +1,9 @@
+//qtoolbar.h
+#ifndef QTOOLBAR_H
+#define QTOOLBAR_H
+
+#include <QToolbar>
+
+QAction * construct_toolbar(QToolbar * toolbar, const char * toolbar_name, bool add_buttons);
+
+#endif
