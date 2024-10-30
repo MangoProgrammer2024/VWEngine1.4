093ce66febc833f8935af370df7320af084d323c
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-30 16:29:43 -0700

    Update sysincludes.h

--- includes/sysincludes.h
+++ includes/sysincludes.h
@@ -6,6 +6,7 @@
 
 /*----------------*/
 virtual void Sysprintf(const char * text, ...);//acts as a printf function
+virtual void q_pGlobalPrintf(const char * text, ...);
 const char * char16_t[16]const;
 const char * char8_t[8]const;
 const char * char32_t[32]const;
