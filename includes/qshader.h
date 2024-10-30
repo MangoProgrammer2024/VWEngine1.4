32560e1d22c4396a46f7ae8f65fb2f7dc1c58082
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 18:30:25 -0700

    Update qshader.h

--- includes/qshader.h
+++ includes/qshader.h
@@ -2,5 +2,29 @@
 #ifndef QSHADER_H
 #define QSHADER_H
 
+#include "qstuff.h"
+
+struct qshader{
+//rgb set values
+  int qR;//R
+  int qG;//G
+  int qB;//B
+    //shader image alphaness
+    float shaderAlpha;
+//shader
+void shader(shader * s, const char * qShadername, bool set);
+//shader type
+enum{
+  normal_shader = 0,
+  transparent_shader = 1
+}shadertype;
+//shader positions
+float _x(qshader);
+float _y(qshader);
+float _z(qshader);
+//shader image extension
+const char * shaderPNG = "*.png"; const char * shaderJPG = "*.jpeg"; const char * dds = "*.dds";
+
+};
 
 #endif
