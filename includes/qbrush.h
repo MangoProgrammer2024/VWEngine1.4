23ea1d87d10e5135c4118fdeee959abbaa822f23
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-29 18:00:24 -0700

    Update qbrush.h

--- includes/qbrush.h
+++ includes/qbrush.h
@@ -17,18 +17,29 @@ public:
     qboolean qbCheckMode(qbrush brush);  qboolean qbCheck_BrushVariable(qbrush * qVariable);
       const char * qb_GlobalBrushName(qbrush * current_qbrush)const;
 
+      //brushpoints
       qbrush * brushpoints( int qbPoints[8] );
+      //allocate brush size
       void qbrush_Alloc(std::size_t * qbrushSize);
+      //brush epair mode
       qbrush * qepair(qbrush * q, const char * key, const char * value);
+      //split brush
       void split_brush(qbrush * q, int addPoints, qboolean qSplitfaces);
+      //add brush to table
       void brush_AddToQTable(qbrush * current_qbrush, qbrushtable qtable);
+        //create brush
         qboolean CreateBrush(qbrush brush);
-
+        //draw brush to grid
         virtual void Brush_DrawToGrid(qbrush * current_brush);
+
 };
 
 typedef struct qbrushtable{
+  typedef void QTABLE_DESCRIPTOR;
+  int iNumber;
 
+    QTABLE_DESCRIPTOR Save_ItemToTable();
+    QTABLE_DESCRIPTOR Create_TableList(qbrushtable * table, const char * name, int MAX_ITEMS_NUM[]);
 
 };
 
