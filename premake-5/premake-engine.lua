37423f380446ba2f043923c57c0f5e15bbaa18b9
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-04 09:38:15 -0600

    Update and rename premade-engine.lua to premake-engine.lua

---
+++ premake-5/premake-engine.lua
@@ -0,0 +1,9 @@
+workspace "VWEngine1.4.sln"
+ configurations { "Debug", "Release" };
+ platforms { "x64", "x86" };
+
+project "VWEngine1.4.sln"
+ kind "C++Application"
+ language "C++"
+ files { "**.cpp", "**.c", "**.lua", "**.h", "**.hpp, "**.py", "**.def", "**.png", "**.jpg", "**.vwi", "**.tga", "**.dll", "**.shader",
+  "**.bat", "**.cmd", "**.map", "**. }
