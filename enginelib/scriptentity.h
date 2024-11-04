53b20bb6109a7109fac9d725e03da3bedf9dd104
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-04 11:24:57 -0600

    Create scriptentity.h

---
+++ enginelib/scriptentity.h
@@ -0,0 +1,19 @@
+//scriptentity.h
+#ifndef SCRIPTENTITY_H
+#define SCRIPTENTITY_H
+
+#include "includes/qentity.h"
+
+template<typename entScript>
+class ScriptEntity{
+public:
+ bool castedScript;
+
+  void scriptentity(qentity * q, ScriptEntity * script){
+      if(q->
+  };
+
+};
+
+
+#endif
