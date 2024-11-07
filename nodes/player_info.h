14c41f58648f3f1ac4889a5442086115392039e1
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-07 13:25:52 -0600

    Create player_info.h

---
+++ nodes/player_info.h
@@ -0,0 +1,37 @@
+//player_info.h
+#ifndef PLAYER_INFO_H
+#define PLAYER_INFO_H
+
+typedef auto instinfo;
+
+struct player_info{
+instinfo construct();
+  enum{
+    PLAYER_INFO_NODE = 0, PLAYER_INFO_PLAYER = 1
+  }nmode;
+
+virtual void EditorMode(player_info * ownernode, player_info node){
+  if(node.EditorMode(ownernode->nmode = PLAYER_INFO_NODE))
+  {
+      int gn_RegisterNode = 1;
+
+       glPrimitive::PrimitType * type = CubeLong(ownernode){ return type };
+        for(int i = 1, i > 0; gn_RegisterNode++)
+          {
+            player_info * info = (player_info*)malloc(std::size_t(player_info));
+             return info;
+          }
+        
+  }
+
+};
+
+
+
+
+};
+
+
+
+
+#endif
