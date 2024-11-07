3deb7376f7e61910685f3bf0db28c856253443e1
Hunter Manko <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-07 10:02:42 -0600

    Update Servers.h

--- Servers/Servers.h
+++ Servers/Servers.h
@@ -3,13 +3,20 @@
 #define SERVERS_H
 
 namespace ServerCodepost{
-
+//server string cstring
 typedef std::string CString;
-
+ //server class socket
  class Socket{
-  public: 
+  public:
+   //cstring new socketname
    ServerCodepost::CString socketname[];
+    //create new socket
+    Socket * CreateSocket(Socket * socket, ServerCodepost::CString socketName[socketname]){
+      Socket socket = new Socket;
+    };
+
     
+
  };
 
 
