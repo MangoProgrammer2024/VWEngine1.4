a5d50606a313ce817ca1f443c5d5214c19925c51
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-08 13:08:26 -0600

    Update waf_info.h

--- enginelib/waf_info.h
+++ enginelib/waf_info.h
@@ -34,11 +34,17 @@ int info_flag;
   const char * BRUSH_MAYJOR; const char * ENTITY_MAYJOR; const char * MODEL_MAYJOR;
 
   void iterate_info(info * i, std::iterator = i(::_iterator_ptr* i), static char iBuffIterator)
-
+/*
+===================
+    WORLDSPAWN
+===================
+*/
    void Worldspawn(info * entity, info infoNode)
     {
+      //if classname 
       if(entity->classname = worldspawn)
        {
+         //iterate node info
          infoNode.iterate_info(entity->classname);
          info * i = (info*)malloc(std::size_t*node(info));
           return i;
@@ -46,8 +52,8 @@ int info_flag;
            //determine worldspawn type
            int classType = info_flag;
 
-            switch(classType)
-              {
+          switch(classType)
+           {
                 case class_brush:
                   entity->nameinfo = info_brush; infoNode.info_brush_flags(entity->BRUSH_MAYJOR, info_flag = 1);
                  break;
@@ -59,10 +65,9 @@ int info_flag;
                 case class_model:
                   entity->nameinfo = info_model; infoNode.info_model_flags(entity->MODEL_MAYJOR, info_flag = 3);
                  break;
-              } 
+          } 
         }       
-    }
-/*******************************************/
+    };
 
   
 
