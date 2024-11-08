967cc399434f87f52c8d147333e8f3d152aca81d
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-08 09:10:59 -0600

    Update gzone.h

--- gzone/gzone.h
+++ gzone/gzone.h
@@ -1,1 +1,30 @@
+//gzone.h
+#pragma once 
+#ifndef GZONE_H
+#define GZONE_H
 
+template<typename _zone>
+typedef struct gzone{
+typedef bool zoneboolean;
+//zone size integer
+int gz_ZoneSize;
+  //enum zone type
+  enum gzZoneType{
+    GZ_SPAWNER_ZONE = 0,
+    GZ_EFFECTS_ZONE = 1,
+    GZ_LIGHT_ZONE = 2
+  };
+
+    //if zone bool
+    zoneboolean zone_on();
+    zoneboolean zone_off();
+
+  std::string zoneName();
+
+    void getZone(std::string zoneKey, std::string zoneValue, std::string zoneInfo);
+
+};
+
+
+
+#endif
