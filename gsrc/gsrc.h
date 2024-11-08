d6a691236d0e31c97202d6e958342f90dbf749e0
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-08 14:53:28 -0800

    Update gsrc.h

--- gsrc/gsrc.h
+++ gsrc/gsrc.h
@@ -1,1 +1,27 @@
+//gsrc.h
+#pragma once
+#ifndef GSRC_H
+#define GSRC_H
 
+#endif
+
+template<typename thread>
+thread();
+/*
+  thread function creates a function
+  kindof works like a void statement
+
+  example:
+
+    main(){
+      mains::load\_dev();
+
+      thread function();
+
+    };
+
+    //call function
+    function(){
+
+    };
+*/
