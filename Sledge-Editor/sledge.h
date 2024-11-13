40d4fb92ca71fb03caeba4e9e2002265d8d4bbc8
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-13 09:34:03 -0600

    Update sledge.h

--- Sledge-Editor/sledge.h
+++ Sledge-Editor/sledge.h
@@ -1,1 +1,20 @@
+/*
+ sledge.h create application entry and gets its contents
+*/
+#pragma once
+#ifndef SLEDGE_H
+#define SLEDGE_H
 
+ namespace __sledge_exe__{
+
+   int __sledge__ = 0;
+
+    const char * texture_browser_directory = "C:\\" const;
+    const char * game_directory = "C:\\" const;
+
+     class g_nbGlobalTextureFilters;
+
+ };
+
+
+#endif
