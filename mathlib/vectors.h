ea8d7622adc230fec14e3bb739a36fc78fc5ccbb
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-14 09:11:43 -0600

    Update vectors.h

--- mathlib/vectors.h
+++ mathlib/vectors.h
@@ -1,43 +1,101 @@
-//vectors.h
+// vectors.h
 #pragma once
 #ifndef VECTORS_H
 #define VECTORS_H
 
-template<typename vec>class vector{
- public:
-//vector type t has an float value
-//vec3_t has an array of 3
-//vec4_t has an array of 4
-//vec6_t has an array of 6
-typedef float vec_t;
-typedef vec_t vec3_t[3];
-typedef vec_t vec4_t[4];
-typedef vec_t vec6_t[6];
- // operator + has float value
- // operator - has unsigned int value
- // operator * has int value
- // operator % has boolean value
- vec operator + (vector, float v);
- vec operator - (vector, unsigned int vsub);
- vec operator * (vector, int vscalar);
- vec operator % (vector, bool vscale);
-  //vector functions
-  //vector snap is a snap to for a vector
-  int vecsnap( int snap, bool snapped )
-  //handles vector to snap to grid
-  void snaptogrid( vec_t * t, vector v);
-  //reset snap returns snap as false
-  bool snapreset(vec_t * current, vector v){
-    if(!current->vec_t vp, v.snaptogrid(current->vec_t))
-     {
-        return false;
-     }
-  };
-  //get vectors matrix position
-  const void vecmatrix(matrix_m * m, vector * v)const;
-  //vector dot product
-  #define VECTOR_DOT (x, y) (x[0] * y[0] + x[1] * y[1] + x[2] * y[2])
+#include <cmath>
+#include <array>
 
+// Templated vector class
+template <typename T>
+class Vector {
+public:
+    // Typedefs for vector types
+    using vec_t = float;
+    using vec3_t = std::array<vec_t, 3>;
+    using vec4_t = std::array<vec_t, 4>;
+    using vec6_t = std::array<vec_t, 6>;
+
+    // Operator overloads for Vector class
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+    Vector operator*(float scalar) const;
+    bool operator%(bool vscale) const;
+
+    // Vector functions
+    // Snaps vector to grid and returns a snapped value
+    int vecSnap(int snap, bool snapped);
+
+    // Snap vector to a grid
+    void snapToGrid(vec_t* t, const Vector& v);
+
+    // Reset snap function, returns false
+    bool snapReset(vec_t* current, const Vector& v) {
+        if (!current) {
+            return false;
+        }
+        snapToGrid(current, v);
+        return false;
+    }
+
+    // Get vector matrix position (requires an external matrix type, assuming it's defined elsewhere)
+    void vecMatrix(const T* matrix, Vector* v) const;
+
+    // Dot product function
+    static float dotProduct(const vec3_t& x, const vec3_t& y) {
+        return x[0] * y[0] + x[1] * y[1] + x[2] * y[2];
+    }
+
+private:
+    T data;  // Vector data (assumed for simplification)
 };
 
-#endif
+// Implementation of operators (examples)
+template <typename T>
+Vector<T> Vector<T>::operator+(const Vector<T>& other) const {
+    Vector<T> result;
+    result.data = data + other.data;  // Placeholder for actual vector addition logic
+    return result;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator-(const Vector<T>& other) const {
+    Vector<T> result;
+    result.data = data - other.data;  // Placeholder for actual vector subtraction logic
+    return result;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator*(float scalar) const {
+    Vector<T> result;
+    result.data = data * scalar;  // Placeholder for actual scalar multiplication
+    return result;
+}
+
+template <typename T>
+bool Vector<T>::operator%(bool vscale) const {
+    // Implement some boolean operation as required
+    return vscale;
+}
+
+template <typename T>
+int Vector<T>::vecSnap(int snap, bool snapped) {
+    // Placeholder logic for vecSnap; implement as needed
+    return snap;
+}
+
+template <typename T>
+void Vector<T>::snapToGrid(vec_t* t, const Vector<T>& v) {
+    // Placeholder for snapping to grid
+    if (t) {
+        *t = std::round(*t);  // Example of rounding
+    }
+}
+
+template <typename T>
+void Vector<T>::vecMatrix(const T* matrix, Vector* v) const {
+    // Placeholder for matrix transformation logic
+    // Actual implementation would depend on the matrix and vector structure
+}
+
+#endif // VECTORS_H
