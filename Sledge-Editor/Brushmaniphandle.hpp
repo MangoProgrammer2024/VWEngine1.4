64dc639ba5db4fc056dd9a68673ac1153d04eb24
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-14 14:07:24 -0800

    Update Brushmaniphandle.hpp

--- Sledge-Editor/Brushmaniphandle.hpp
+++ Sledge-Editor/Brushmaniphandle.hpp
@@ -9,9 +9,12 @@
 template<typename _BrushHandleModule>
 class BrushModule{
   public:
-
-  _BrushHandleModule operator ++(const std::string& gn_bSelectedBrush, std::size_t * gBrushSize, std::array<BrushSize_Plane[]>) = 0;
+  //handle brush size
+  _BrushHandleModule operator ++g_nbBrushSize(const std::string& gn_bSelectedBrush, std::size_t * gBrushSize, std::array<BrushSize_Plane[]>) = 0;
+  //handles deleted brush data
   const _BrushHandleModule operator $null(const std::string&& gn_bDeletedBrush, std::size_t * gn_bBrushSize = NULL, std::array<NULL>)const;
+  //handles brush bin
+  const _BrushHandleModule operator %g_nbBin(const std::string& g_nbBrushName = g_nbGlobal_Key, std::size_t * g_nbBrushSize = sizeof(brush_t * b), std::array<std::size_t * brush>, const char&&saveFile)const;
 };
 
 
