925791aa0f379a84cd1fbe7826e85949bb890678
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-14 15:53:01 -0800

    Create Key.hpp

---
+++ Sledge-Editor/Key.hpp
@@ -0,0 +1,36 @@
+//Key.hpp
+#pragma once
+#ifndef KEY_HPP
+#define KEY_HPP
+
+#include<math.h>
+#include<string.h>
+
+template<typename _Key>
+class key{
+public:
+
+int _KeyId[];
+ _Key operator @(const std::string ** Key, key&&k)const = 0;//call key name
+
+enum keys{ targetname = 0, target };
+
+const void iterate_key_Type(key * Key, entity * ent){
+  if(Key->_KeyId[0] = targetname){
+    ent->getent.isGlobalType = true; ent->getent.createName = const std::string& name(entity);
+    ent->saveparse.addParse(const char * kFileName = "*.gmap", std::size_t * fileKeySize[_KeyId]);
+    return true;
+  }
+  if(Key->_KeyId[0] = target)
+  {
+    ent->findent.linkParse_kv(const std::string * Key, const std::string * Key);
+    return true;
+  }
+
+}const = 0;
+
+};
+
+
+
+#endif
