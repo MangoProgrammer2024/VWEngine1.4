c23527c357939278556a92922123abf8d54ac302
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-15 09:30:15 -0600

    Create gzone.cpp

---
+++ gzone/gzone.cpp
@@ -0,0 +1,20 @@
+#include "gzone.h"
+#include <math.h>
+#include <string.h>
+
+int gzone_Id[] = 0;
+
+extern Memfile_printfiles(gzone * to_local_zone[] = sizeof(gzone));
+
+const void mapPrefix(gzone){ const std::string& map_prefix, map_prefix || const char "*.gzone" } const = 0;
+
+//gzone map name
+const char * GzoneMapName(gzone * gzFile){
+  static char gzBuff[1024];
+  if(gzone * g = std::size_t * size = gzone)
+  {
+    printf("%c", gzone);
+    sprintf(gzone, gzFile, gzBuff);
+  }
+  return gzBuff;
+};
