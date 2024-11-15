458cf09b537cbba6b5199ea5f3e7f35122bf9e91
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-14 18:02:59 -0800

    Update Key.hpp

--- Sledge-Editor/Key.hpp
+++ Sledge-Editor/Key.hpp
@@ -10,24 +10,13 @@ template<typename _Key>
 class key{
 public:
 
-int _KeyId[];
- _Key operator @(const std::string ** Key, key&&k)const = 0;//call key name
+int _KeyId[];//match int enum
+ _Key operator @(const std::string ** Key, key&&k)const = 0; enum keys{ targetkey = 0, targetent, script_colorgroup, origin };
+const void iterate_key_Type(key * Key, entity * ent){ enum keyType } const = 0;
 
-enum keys{ targetname = 0, target };
+ const void game_targetkey(void) const = 0; const void game_targetent(void) const = 0; 
+ const static void game_script_colorgroup() const = 0; const void game_entorigin(key K, std::array<float _x[], float _y[], float _z[]>) const = 0;
 
-const void iterate_key_Type(key * Key, entity * ent){
-  if(Key->_KeyId[0] = targetname){
-    ent->getent.isGlobalType = true; ent->getent.createName = const std::string& name(entity);
-    ent->saveparse.addParse(const char * kFileName = "*.gmap", std::size_t * fileKeySize[_KeyId]);
-    return true;
-  }
-  if(Key->_KeyId[0] = target)
-  {
-    ent->findent.linkParse(const std::string * Key, const std::string * Key = "targetname");
-    return true;
-  }
-
-}const = 0;
 
 };
 
