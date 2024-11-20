4b8f50f320c1422a7eda2437ad98c26e87ed0add
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-20 14:08:29 -0800

    Create Brush.cpp

---
+++ Sledge-Editor/Brush.cpp
@@ -0,0 +1,63 @@
+#include"Sledge-Editor/Brushmaniphandle.hpp"
+#include"Sledge-Editor/Filters.h"
+#include"Sledge-Editor/Grid.hpp"
+#include"Sledge-Editor/Key.hpp"
+#include"mathlib/matrix.h"
+#include"mathlib/vectors.h"
+#include"includes/qstuff.h"
+#include"Sledge-Editor/Brush.hpp"
+#include"includes/sysincludes.h"
+#include<vector>
+#include "Sledge-Editor/sledge.h"
+#include "Sledge-Editor/Preferences.hpp"
+#include"Sledge-Editor/PreferencesWnd.hpp"
+
+#define BRUSH_MAYJOR "Brush" 000
+
+const std::string& BrushType;
+
+//brush preferences aka brush type
+Brush&& brushPreferences(SledgePreferencesModule&& preferences, PreferencesWnd&& moduleLoad) {
+	__sledge_exe__::sledge_GetAPIModule(); preferences.brushType; 
+		moduleLoad.prefrencesbrushTypeTextBox().find(BrushType == preferences.brushType); 
+		std::size_t&& moduleSize(Brush&& brushPreferences);
+			if (preferences.brushType == "WaFBrush") { __sledge_exe__::__sledge__entry__id__; }
+};
+
+//brush setup
+Brush* brushSetup(Brush&& brush, const std::string&& loadType) {
+	int eBrushId = 0; std::size_t* brushIdSize = nullptr;
+		if ( brushSetup ){
+			Brush* brush = (Brush*)brush->g_nbBrushMallocModule(sizeof(Brush)); Sysprintf("loadType", "Brush", "%s");
+		}
+};
+
+//brush info
+/*BRUSHED (classname)(brush)
+ Brush info is classname
+*/
+Brush&& brushInfo(Brush&& brush, const std::string&& brush_info, const char * brushModule) {
+	if (brush.BrushCreated(brush) == true) {
+		brush_info == "classname", "brush"; brush_info.find(brush_info); std::size_t * brush_info_max;
+	}
+};
+//brush misc stuff
+std::size_t brush_faceSize; std::size_t brush_pointSize; std::uint32_t faceSize = 0;
+//brush vertex setup
+template<typename BrushVertex>
+BrushVertex* BrushVerts(const Brush * cubiod, const Vector<BrushVertex>&& vertex) {
+	std::size_t brushvertex != brush_pointSize; return vertex.dotProduct();
+};
+//brush face size failed
+void brushSizefailed(Brush* brush) {
+	if (!(brush_faceSize == 0)) { brush_faceSize != faceSize; };
+	return;
+};
+//return brush size
+template<typename PrimitSize>
+Brush* returnSize() {
+	Brush* b; std::size_t BrushSize; 
+		if( BrushSize ){ Brush std::array<PrimitSize, BrushSize>Brush; return BrushSize; };
+
+		return b;
+};
