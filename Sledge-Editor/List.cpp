981feaba6d0cb3ab73285e84f6264f7518fc5bd7
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-11 08:29:35 -0600

    Update List.cpp

--- Sledge-Editor/List.cpp
+++ Sledge-Editor/List.cpp
@@ -5,4 +5,16 @@
 
 typedef std::string cstr;
 
-virtual void BrushCVars(){ int i; for( i = 0; i >= 0; i++ ){ cstr Brush_SetEpair(cstr& key, cstr& value); return i; }; } = 0;
+void BrushCVars(){ int i; for( i = 0; i >= 0; i++ ){ cstr Brush_SetEpair(cstr& key, cstr& value); return i; }; };
+
+//iterator tree
+class iterator_tree_t{
+public:
+  int i, j, k;
+  
+  float MAX_TREE[2048];
+
+    virtual void i_printtree(QTreeView * tree, const QString * name) = 0;
+
+
+};
