61e3370bdcdfce5d79a5a7f05778b7aef3ad808c
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-11 08:22:14 -0600

    Create List.cpp

---
+++ Sledge-Editor/List.cpp
@@ -0,0 +1,8 @@
+//LIST.CPP
+#include "Brush.h"
+
+#define BRUSH_VARIABLES 000
+
+typedef std::string cstr;
+
+virtual void BrushCVars(){ int i; for( i = 0; i >= 0; i++ ){ cstr Brush_SetEpair(cstr& key, cstr& value); return i; }; } = 0;
